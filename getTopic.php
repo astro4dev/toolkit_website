0590b8657967d36b7fcbbccc9783d723131f74fd
Dr. Paul A. Wilson <paultheastronomer@protonmail.com>
2017-08-01 15:05:52 +0200

    added basic language support

--- getTopic.php
+++ getTopic.php
@@ -16,14 +16,14 @@ if (!$con) {
 }
 
 if (empty($keyword)) {
-$query_assessments  = "SELECT title, name, affiliation, author_link, author_img, links, author_id, assessment_id FROM authors, assessments, authors__assessments WHERE (authors.id=author_id AND assessments.Id = assessment_id);";
-$query_courses      = "SELECT title, name, affiliation, author_link, author_img, links, author_id, course_id FROM authors, courses, authors__courses WHERE (authors.id=author_id AND courses.Id = course_id);";
-$query_examples     = "SELECT title, name, affiliation, author_link, author_img, links, author_id, example_id FROM authors, examples, authors__examples WHERE (authors.id=author_id AND examples.Id = example_id);";
+$query_assessments  = "SELECT title, name, affiliation, author_link, author_img, links, author_id, assessment_id, language FROM authors, assessments, authors__assessments WHERE (authors.id=author_id AND assessments.Id = assessment_id);";
+$query_courses      = "SELECT title, name, affiliation, author_link, author_img, links, author_id, course_id, language FROM authors, courses, authors__courses WHERE (authors.id=author_id AND courses.Id = course_id);";
+$query_examples     = "SELECT title, name, affiliation, author_link, author_img, links, author_id, example_id, language FROM authors, examples, authors__examples WHERE (authors.id=author_id AND examples.Id = example_id);";
 }
 else {
-$query_assessments  = "SELECT title, name, affiliation, author_link, author_img, links, author_id, assessment_id FROM authors, assessments, authors__assessments WHERE (authors.id=author_id AND assessments.Id = assessment_id) AND (IF(LENGTH('".$keyword."') > 0, assessments.title LIKE '%".$keyword."%', 0));";
-$query_courses      = "SELECT title, name, affiliation, author_link, author_img, links, author_id, course_id FROM authors, courses, authors__courses WHERE (authors.id=author_id AND courses.Id = course_id) AND (IF(LENGTH('".$keyword."') > 0, courses.title LIKE '%".$keyword."%', 0));";
-$query_examples     = "SELECT title, name, affiliation, author_link, author_img, links, author_id, example_id FROM authors, examples, authors__examples WHERE (authors.id=author_id AND examples.Id = example_id) AND (IF(LENGTH('".$keyword."') > 0, examples.title LIKE '%".$keyword."%', 0));";
+$query_assessments  = "SELECT title, name, affiliation, author_link, author_img, links, author_id, assessment_id, language FROM authors, assessments, authors__assessments WHERE (authors.id=author_id AND assessments.Id = assessment_id) AND (IF(LENGTH('".$keyword."') > 0, assessments.title LIKE '%".$keyword."%', 0));";
+$query_courses      = "SELECT title, name, affiliation, author_link, author_img, links, author_id, course_id, language FROM authors, courses, authors__courses WHERE (authors.id=author_id AND courses.Id = course_id) AND (IF(LENGTH('".$keyword."') > 0, courses.title LIKE '%".$keyword."%', 0));";
+$query_examples     = "SELECT title, name, affiliation, author_link, author_img, links, author_id, example_id, language FROM authors, examples, authors__examples WHERE (authors.id=author_id AND examples.Id = example_id) AND (IF(LENGTH('".$keyword."') > 0, examples.title LIKE '%".$keyword."%', 0));";
 }
 
 $search_assessments = mysqli_query($con, $query_assessments);
@@ -36,10 +36,12 @@ $show_all   = mysqli_query($con, $query_all_courses);
 
 if( mysqli_num_rows($search_examples)) {
     echo "<tr>
-    <th colspan='2'>Examples:".$show."</th>
+    <th colspan='3'>Examples:".$show."</th>
     </tr>";
+    echo "<tr><td><i>Language</i></td><td><i>Title</i></td><td><i>Author</i></td></tr>";
     while($row_search_query = mysqli_fetch_array($search_examples)) {
-        echo "<tr><td><a href=\"" . $row_search_query['links'] . "\" target=\"_blank\"><i>" . $row_search_query['title'] . "</i></a></td>";
+        echo "<tr><td>". $row_search_query['language'] . "</td>";
+        echo "<td><a href=\"" . $row_search_query['links'] . "\" target=\"_blank\"><i>" . $row_search_query['title'] . "</i></a></td>";
         echo "<td><a href=\"" . $row_search_query['author_link'] . "\" target=\"_blank\"><i>" . $row_search_query['name'] . "</i></a></td>";
         }
     echo "</tr>";
@@ -49,10 +51,12 @@ if( mysqli_num_rows($search_examples)) {
 
 if( mysqli_num_rows($search_courses)) {
     echo "<tr>
-    <th colspan='2'>Courses:</th>
+    <th colspan='3'>Courses:</th>
     </tr>";
+    echo "<tr><td><i>Language</i></td><td><i>Title</i></td><td><i>Author</i></td></tr>";
     while($row_search_query = mysqli_fetch_array($search_courses)) {
-        echo "<tr><td> <a href=\"" . $row_search_query['links'] . "\" target=\"_blank\"><i>" . $row_search_query['title'] . "</i></a></td>";
+        echo "<tr><td>". $row_search_query['language'] . "</td>";
+        echo "<td> <a href=\"" . $row_search_query['links'] . "\" target=\"_blank\"><i>" . $row_search_query['title'] . "</i></a></td>";
         echo "<td><a href=\"" . $row_search_query['author_link'] . "\" target=\"_blank\"><i>" . $row_search_query['name'] . "</i></a></td>";
         #echo "<td><a href=\"" . $row_search_query['author_link'] . "\" target=\"_blank\"><i>#SQL</i></a> / <a href=\"" . $row_search_query['author_link'] . "\" target=\"_blank\"><i>#ASTR</i></a></td>";
         }
@@ -61,10 +65,12 @@ if( mysqli_num_rows($search_courses)) {
 
 if( mysqli_num_rows($search_assessments) ) {
     echo "<tr>
-    <th colspan='2'>Assessments:</th>
+    <th colspan='3'>Assessments:</th>
     </tr>";
+    echo "<tr><td><i>Language</i></td><td><i>Title</i></td><td><i>Author</i></td></tr>";
     while($row_search_query = mysqli_fetch_array($search_assessments)) {
-        echo "<tr><td> <a href=\"" . $row_search_query['links'] . "\" target=\"_blank\"><i>" . $row_search_query['title'] . "</i></a></td>";
+        echo "<tr><td>". $row_search_query['language'] . "</td>";
+        echo "<td> <a href=\"" . $row_search_query['links'] . "\" target=\"_blank\"><i>" . $row_search_query['title'] . "</i></a></td>";
         echo "<td><a href=\"" . $row_search_query['author_link'] . "\" target=\"_blank\"><i>" . $row_search_query['name'] . "</i></a></td>";
         }
     echo "</tr>";
@@ -79,6 +85,22 @@ mysqli_free_result($search_examples);
 
 mysqli_close($con);
 ?>
+
+
+<script type="text/javascript">
+    
+$("#idOfElement").on('click', function(){
+
+    $.ajax({
+       url: 'pathToPhpFile.php',
+       dataType: 'json',
+       success: function(data){
+            //data returned from php
+       }
+    });
+)};
+    
+</script>
 
 
 <!-- Work in progress
--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 	<head>
 	<?php include 'header.php';?>
 	</head>
+
 	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
 	<body>
 
@@ -17,38 +18,77 @@
 				<!-- Header -->
 					<header id="header" class="alt">
 						<span class="logo"><img src="images/DST_logo_250px.png" alt="" /></span>
-						<h1>Astronomy & Data Science Toolkit</h1>
-						<p>Connecting Astronomy and Data Science</p>
+						
+						<!-- ENGLISH -->
+						<h1 lang="en">Astronomy & Data Science Toolkit</h1>
+						<p lang="en">Connecting Astronomy and Data Science</p>
+
+						<!-- SPANISH -->
+						<h1 lang="es">Toolkit de Astronomía y Ciencia de Datos</h1>
+						<p lang="es">Conexión de la astronomía y la ciencia de los datos</p>
+
 					</header>
 
 				<!-- Nav -->
 					<nav id="nav">
 						<ul>
-							<li><a href="#intro" class="active">Introduction</a></li>
-							<li><a href="#about">About the Toolkit</a></li>
-							<li><a href="toolkit.php">View Toolkit</a></li>
-							<li><a href="#contribute">Contribute</a></li>
-							<li><a href="#footer">Contact</a></li>
+
+							<!-- ENGLISH -->
+							<li><a href="#intro" class="active" lang="en">Introduction</a></li>
+							<li><a href="#about" lang="en">About the Toolkit</a></li>
+							<li><a href="toolkit.php" lang="en">View Toolkit</a></li>
+							<li><a href="#contribute" lang="en">Contribute</a></li>
+							<li><a href="#footer" lang="en">Contact</a></li>
+
+							<!-- SPANISH -->
+							<li><a href="#intro" class="active" lang="es">Introducción</a></li>
+							<li><a href="#about" lang="es">Sobre el Toolkit</a></li>
+							<li><a href="toolkit.php" lang="es">Ver Toolkit</a></li>
+							<li><a href="#contribute" lang="es">Contribuir</a></li>
+							<li><a href="#footer" lang="es">Contáctenos</a></li>
+
+							<li>
+							<select id="lang-switch" class="fa-select">
+							  <option value="en" selected>&#xf1ab; English</option>
+							  <option value="es" >&#xf1ab; Espanol</option>
+							</select>
+							</li>
+
 						</ul>
 					</nav>
 
+
+
 				<!-- Main -->
 					<div id="main">
-					<a href="https://github.com/astro4dev" class="ribbon bg-purple" >Alpha version</a>
+
+					<!-- ENGLISH -->
+					<a href="https://github.com/astro4dev" class="ribbon bg-purple" lang="en">Alpha version</a>
+
+					<!-- SPANISH -->
+					<a href="https://github.com/astro4dev" class="ribbon bg-purple" lang="es">Versión alfa</a>
 
 						<!-- Introduction -->
 							<section id="intro" class="main">
 								<div class="spotlight">
 									<div class="content">
 										<header class="major">
-											<h2>Overall objective</h2>
+
+											<!-- ENGLISH -->
+											<h2 lang="en">Overall objective</h2>
+
+											<!-- SPANISH -->
+											<h2 lang="es">Ayúdanos a traducir al español</h2>
+
 										</header>
-										<p>The overall objective of this toolkit is to provide a foundation for a “common language” between data science and astronomy communities. The toolkit should help astronomers get a sense of the breadth of data science (as a field and in its applications) and to see where their skills and knowledge fit into this wider view; it should help data scientists and those in need of data scientists (e.g. companies, NGOs) see what astronomers can contribute.</p>
-										<!--
-										<ul class="actions">
-											<li><a href="generic.html" class="button">Learn More</a></li>
-										</ul>
-										-->
+
+										<!-- ENGLISH -->
+										<p lang="en">The overall objective of this toolkit is to provide a foundation for a “common language” between data science and astronomy communities. The toolkit should help astronomers get a sense of the breadth of data science (as a field and in its applications) and to see where their skills and knowledge fit into this wider view; it should help data scientists and those in need of data scientists (e.g. companies, NGOs) see what astronomers can contribute.</p>
+
+										<!-- SPANISH -->
+										<p lang="es">Necesitamos ayuda para traducir. Por favor, eche un vistazo a cómo se puede traducir <a href="https://github.com/astro4dev/OAD-Data-Science-Toolkit/blob/master/CONTRIBUTING.md" target="_blank">aquí</a>.</p>
+
+
 									</div>
 
 									<span class="image"><img src="images/horsehead_920px.jpg" alt="" /></span>
@@ -58,31 +98,62 @@
 						<!-- About Section -->
 							<section id="about" class="main special">
 								<header class="major">
-									<h2>About the toolkit</h2>
+
+									<!-- ENGLISH -->
+									<h2 lang="en">About the toolkit</h2>
+
+									<!-- SPANISH -->
+									<h2 lang="es">Sobre el Toolkit</h2>
+
 								</header>
 								<ul class="features">
 									<li>
 										<span class="icon major style1 fa-code"></span>
-										<h3>Open Source</h3>
-										<p>The entire toolkit is released as Open Source under the GPLv3 license. This includes: the content, the tools for updating it and this website.</p>
+										<!-- ENGLISH -->
+										<h3 lang="en">Open Source</h3>
+										<p lang="en">The entire toolkit is released as Open Source under the GPLv3 license. This includes: the content, the tools for updating it and this website.</p>
+
+										<!-- SPANISH -->
+										<h3 lang="es">Open Source</h3>
+										<p lang="es">The entire toolkit is released as Open Source under the GPLv3 license. This includes: the content, the tools for updating it and this website.</p>
+
 									</li>
 									<li>
 										<span class="icon major style3 fa-users"></span>
-										<h3>Community driven</h3>
-										<p>The toolkit is maintained by volunteers from astronomy and data science communities.</p>
+										
+										<!-- ENGLISH -->
+										<h3 lang="en">Community driven</h3>
+										<p lang="en">The toolkit is maintained by volunteers from astronomy and data science communities.</p>
+
+										<!-- SPANISH -->
+										<h3 lang="es">Community driven</h3>
+										<p lang="es">The toolkit is maintained by volunteers from astronomy and data science communities.</p>
+
 									</li>
 									<li>
 										<span class="icon major style5 fa-universal-access"></span>
-										<h3>Accessibility</h3>
-										<p>The toolkit is accessible free of charge. Contact us if you have problems accessing the toolkit.</p>
+
+										<!-- ENGLISH -->
+										<h3 lang="en">Accessibility</h3>
+										<p lang="en">The toolkit is accessible free of charge. Contact us if you have problems accessing the toolkit.</p>
+
+										<!-- SPANISH -->
+										<h3 lang="es">Accessibility</h3>
+										<p lang="es">The toolkit is accessible free of charge. Contact us if you have problems accessing the toolkit.</p>
+
 									</li>
 								</ul>
 								
-								<p>
-								The focus of the toolkit is to provide high quality teaching materials in the form of examples, courses and assessments which you are welcome to use for your teaching or perhaps when organising a workshop or a summer school.</p>
+								<!-- ENGLISH -->
+								<p lang="en">The focus of the toolkit is to provide high quality teaching materials in the form of examples, courses and assessments which you are welcome to use for your teaching or perhaps when organising a workshop or a summer school.</p>
 
-								<p>
-								The toolkit is aimed at specifically connecting the fields of astronomy and data science and is not meant to compete with massive open online courses such as <a href="https://www.edx.org/" target="_blank">edX</a> or <a href="https://www.coursera.org/" target="_blank">Coursera</a>.</p>
+								<p lang="en">The toolkit is aimed at specifically connecting the fields of astronomy and data science and is not meant to compete with massive open online courses such as <a href="https://www.edx.org/" target="_blank">edX</a> or <a href="https://www.coursera.org/" target="_blank">Coursera</a>.</p>
+
+
+								<!-- SPANISH -->
+								<p lang="es">The focus of the toolkit is to provide high quality teaching materials in the form of examples, courses and assessments which you are welcome to use for your teaching or perhaps when organising a workshop or a summer school.</p>
+
+								<p lang="es">The toolkit is aimed at specifically connecting the fields of astronomy and data science and is not meant to compete with massive open online courses such as <a href="https://www.edx.org/" target="_blank">edX</a> or <a href="https://www.coursera.org/" target="_blank">Coursera</a>.</p>								
 
 								<footer class="major">
 									<ul class="actions">
@@ -93,56 +164,110 @@
 
 							<section id="contribute" class="main special">
 								<header class="major">
-									<h2>Help contribute</h2>								
 									
-									<p>The toolkit is in its infancy and for it to be a success we need people to help us create and share content. We are always looking for people keen on contributing.</p>
+									<!-- ENGLISH -->
+									<h2 lang="en">Help contribute</h2>								
+									
+									<p lang="en">The toolkit is in its infancy and for it to be a success we need people to help us create and share content. We are always looking for people keen on contributing.</p>
+
+									<!-- SPANISH -->
+									<h2 lang="es">Help contribute</h2>								
+									
+									<p lang="es">The toolkit is in its infancy and for it to be a success we need people to help us create and share content. We are always looking for people keen on contributing.</p>
 
 
 								<ul class="statistics">
 									<li class="style1">
 										<span class="icon fa-file-code-o"></span>
-										<strong>4</strong> Examples
+
+										<strong>4</strong>
+
+										<!-- ENGLISH -->
+										<i lang="en">Examples</i>
+
+										<!-- SPANISH -->
+										<i lang="es">Ejemplos</i>
+
 									</li>
 									<li class="style2">
 										<span class="icon fa-pencil-square-o"></span>
-										<strong>1</strong><a href="https://github.com/astro4dev/OAD-Data-Science-Toolkit/blob/master/Teaching%20Materials/Programming/Python/Assessments/Pre-assessment.md" target="_blank">Assessment</a>
+										<strong>1</strong>
+										
+										<!-- ENGLISH -->
+										<i lang="en"><a href="https://github.com/astro4dev/OAD-Data-Science-Toolkit/blob/master/Teaching%20Materials/Programming/Python/Assessments/Pre-assessment.md" target="_blank">Assessment</a></i>
+
+										<!-- SPANISH -->
+										<i lang="es"><a href="https://github.com/astro4dev/OAD-Data-Science-Toolkit/blob/master/Teaching%20Materials/Programming/Python/Assessments/Pre-assessment.md" target="_blank">Evaluación</a></i>
+
 									</li>
 									<li class="style3">
 										<span class="icon fa-graduation-cap"></span>
-										<strong>4</strong> Courses
+										<strong>4</strong>
+
+										<!-- ENGLISH -->
+										<i lang="en">Courses</i>
+
+										<!-- SPANISH -->
+										<i lang="es">Cursos</i>
+
 									</li>
 									<li class="style4">
 										<span class="icon fa-code-fork"></span>
-										<strong>8</strong><a href="https://github.com/astro4dev/OAD-Data-Science-Toolkit/network/members" target="_blank">Forks</a>
+										<strong>8</strong>
+
+										<!-- ENGLISH -->
+										<i lang="en"><a href="https://github.com/astro4dev/OAD-Data-Science-Toolkit/network/members" target="_blank">Forks</a></i>
+									
+										<!-- SPANISH -->
+										<i lang="es"><a href="https://github.com/astro4dev/OAD-Data-Science-Toolkit/network/members" target="_blank">Tenedores</a></i>
+
 									</li>
 									<li class="style5">
 										<span class="icon fa-user"></span>
-										<strong>9</strong><a href="https://github.com/astro4dev/OAD-Data-Science-Toolkit" target="_blank">Contributors</a>
+										<strong>9</strong>
+
+										<!-- ENGLISH -->
+										<i lang="en"><a href="https://github.com/astro4dev/OAD-Data-Science-Toolkit" target="_blank">Contributors</a></i>
+
+										<!-- SPANISH -->
+										<i lang="es"><a href="https://github.com/astro4dev/OAD-Data-Science-Toolkit" target="_blank">Colaboradores</a></i>
+
 									</li>
 								</ul>
 
-								<h3>Teaching materials</h3>
-								<!-- <img src="images/logo.svg" alt="" /> -->
-								<p class="content">If you have ever taught astronomy using data science techniques or you are a data scientist who has used astronomy examples to teach data science, please consider sharing your teaching materials. You can either directly contribute to <a href="https://github.com/astro4dev/OAD-Data-Science-Toolkit" target="_blank">github</a> or send us the materials directly via email.</p>
+								<!-- ENGLISH -->
+								<h3 lang="en">Teaching materials</h3>
+								<p class="content" lang="en">If you have ever taught astronomy using data science techniques or you are a data scientist who has used astronomy examples to teach data science, please consider sharing your teaching materials. You can either directly contribute to <a href="https://github.com/astro4dev/OAD-Data-Science-Toolkit" target="_blank">github</a> or send us the materials directly via email.</p>
 
-								<h3>Assessments</h3>
-								<!-- <img src="images/logo.svg" alt="" /> -->
-								<p class="content">Few Astronomy summer schools and extra-curricular programmes use any form of assessment. This deprives students of the opportunity to both experience and demonstrate their achievements; and doesn't allow the ability to show others that they achieved what they intended. The toolkit strives towards providing high quality pre- and post-assessments.</p>
+								<!-- SPANISH -->
+								<h3 lang="es">Teaching materials</h3>
+								<p class="content" lang="es">If you have ever taught astronomy using data science techniques or you are a data scientist who has used astronomy examples to teach data science, please consider sharing your teaching materials. You can either directly contribute to <a href="https://github.com/astro4dev/OAD-Data-Science-Toolkit" target="_blank">github</a> or send us the materials directly via email.</p>
 
-								<h3>Coding and Content</h3>
-								<!-- <img src="images/logo.svg" alt="" /> -->
-								<p class="content">This website has been created using a lot of different tools: HTML5, CSS3, PHP, MySQL, AJAX, jQuery, Shell, JavaScript and Python. If you have experience in any of these tools or perhaps would like to improve the content on this website please help contribute. The entire website can be found <a href="https://github.com/astro4dev/toolkit_website" target="_blank">here</a>.</p>
 
+
+								<!-- ENGLISH -->
+								<h3 lang="en">Assessments</h3>
+								<!-- <img src="images/logo.svg" alt="" /> -->
+								<p class="content" lang="en">Few Astronomy summer schools and extra-curricular programmes use any form of assessment. This deprives students of the opportunity to both experience and demonstrate their achievements; and doesn't allow the ability to show others that they achieved what they intended. The toolkit strives towards providing high quality pre- and post-assessments.</p>
+
+								<!-- SPANISH -->
+								<h3 lang="es">Assessments</h3>
+								<!-- <img src="images/logo.svg" alt="" /> -->
+								<p class="content" lang="es">Few Astronomy summer schools and extra-curricular programmes use any form of assessment. This deprives students of the opportunity to both experience and demonstrate their achievements; and doesn't allow the ability to show others that they achieved what they intended. The toolkit strives towards providing high quality pre- and post-assessments.</p>
+
+
+
+								<!-- ENGLISH -->
+								<h3 lang="en">Coding and Content</h3>
+								<!-- <img src="images/logo.svg" alt="" /> -->
+								<p class="content" lang="en">This website has been created using a lot of different tools: HTML5, CSS3, PHP, MySQL, AJAX, jQuery, Shell, JavaScript and Python. If you have experience in any of these tools or perhaps would like to improve the content on this website please help contribute. The entire website can be found <a href="https://github.com/astro4dev/toolkit_website" target="_blank">here</a>.</p>
+
+								<!-- SPANISH -->
+								<h3 lang="es">Coding and Content</h3>
+								<!-- <img src="images/logo.svg" alt="" /> -->
+								<p class="content" lang="es">This website has been created using a lot of different tools: HTML5, CSS3, PHP, MySQL, AJAX, jQuery, Shell, JavaScript and Python. If you have experience in any of these tools or perhaps would like to improve the content on this website please help contribute. The entire website can be found <a href="https://github.com/astro4dev/toolkit_website" target="_blank">here</a>.</p>
 								</header>
 
-								<!-- Get Started
-								<footer class="major">
-									<ul class="actions">
-										<li><a href="generic.html" class="button special">Get Started</a></li>
-										<li><a href="generic.html" class="button">Learn More</a></li>
-									</ul>
-								</footer>
-								-->
 							</section>
 
 					</div>
@@ -153,15 +278,6 @@
 			</footer>
 
 			</div>
-
-		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.scrollex.min.js"></script>
-			<script src="assets/js/jquery.scrolly.min.js"></script>
-			<script src="assets/js/skel.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
-			<script src="assets/js/main.js"></script>
 
 	</body>
 </html>
